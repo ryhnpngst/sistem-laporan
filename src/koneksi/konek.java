5134a03eb74383e53d7a59a15e795f554cdb810a
ryhnpngst <rayhanwibowo24@gmail.com>
2021-12-27 01:01:57 +0700

    Hihihi

--- src/koneksi/konek.java
+++ src/koneksi/konek.java
@@ -4,23 +4,10 @@
  */
 package koneksi;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 /**
  *
  * @author Lenovo
  */
 public class konek {
-    private static Connection koneksi;
     
-    public static Connection GetConnection()throws SQLException{
-        if(koneksi==null){
-            new Driver();
-            koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistem_tingkat_kriminal", "root", "");
-        }
-        return koneksi;
-    }
 }
