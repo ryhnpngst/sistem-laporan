eaaeb9a20bd79b757ff0906a9630d733d6f7e6dc
ryhnpngst <rayhanwibowo24@gmail.com>
2021-12-27 22:30:54 +0700

    Update Koneksi Database

--- src/koneksi/konek.java
+++ src/koneksi/konek.java
@@ -4,10 +4,28 @@
  */
 package koneksi;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Lenovo
  */
 public class konek {
-    
+    Connection con = null;
+    public static Connection konek(){
+        String driver = "com.mysql.jdbc.Driver";
+        String host = "jdbc:mysql://localhost/sistem_tingkat_kriminal";
+        String user = "root";
+        String password = "";
+        try{
+            Class.forName(driver);
+            Connection con = (Connection) DriverManager.getConnection(host, user, password);
+            return con;
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
 }
